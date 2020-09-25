acfcb52c41800e0914c030265eac085c834bfc2b
momoislam8849 <shampa15-8849@diu.edu.bd>
2020-09-25 11:05:21 +0600

    new files

--- theblog/views.py
+++ theblog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.views.generic import ListView,DetailView
 # Create your views here.
 
-def home(request):
-	return render(request, 'home.html', {})
+#def home(request):
+#	return render(request, 'home.html', {})
+
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+	model = Post
+	template_name = 'article_detail.html'
+		
