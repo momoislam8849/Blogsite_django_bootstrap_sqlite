2428db006836221160696a72b97ab8b548b0d620
momoislam8849 <shampa15-8849@diu.edu.bd>
2020-10-07 19:29:07 +0600

    New pages

---
+++ theblog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = ('title','author','body')
+
+		widgets = {
+			'title': forms.TextInput(attrs={'class':'form-control','placeholder':'Give your title'}),
+			'author': forms.Select(attrs={'class':'form-control','placeholder':'Author name'}),
+			'body': forms.Textarea(attrs={'class':'form-control','placeholder':'Write your blog'}),
+
+
+
+
+		}
+		
--- theblog/models.py
+++ theblog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,3 +13,7 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title + ' | ' + str(self.author)  #shows title and author in admin panel
 
+
+	def get_absolute_url(self):
+		#return reverse('article_detail', args=(str(self.id)))
+		return reverse('home')
